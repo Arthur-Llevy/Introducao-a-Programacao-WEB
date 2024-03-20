e69b6f787108fb0826b2b5881fd00500eb74801d
Arthur Levy <120432923+Arthur-Llevy@users.noreply.github.com>
2024-03-20 23:01:18 +0000

    Outro commit teste

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ const randomNumber = Math.floor(Math.random() * 500 + 1);
 console.log(randomNumber);
 
 const tryNumber = () => {
-    const userNumber = document.getElementById("userNumber");
+    const userNumber = document.getElementById("userNumber").value;
+
+    if (userNumber != randomNumber) {
+        const message = userNumber < randomNumber ? "menor" : "maior";
+        return alert(`Você errou. O número que você digitou é ${message}`);
+    }
+
+    return alert('Você acertou!');
 }
